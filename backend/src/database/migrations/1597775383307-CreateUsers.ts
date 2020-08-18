7854b29f82d881679dea409a00da4d6ff7c15a0d
Leandro Soares <lsoares199658@gmail.com>
2020-08-18 20:11:27 -0300

    feat: register user

--- backend/src/database/migrations/1597775383307-CreateUsers.ts
+++ backend/src/database/migrations/1597775383307-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointments1597763812973
-  implements MigrationInterface {
+export default class CreateUsers1597775383307 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,13 +14,21 @@ export default class CreateAppointments1597763812973
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
-            isNullable: false,
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -39,6 +46,6 @@ export default class CreateAppointments1597763812973
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
