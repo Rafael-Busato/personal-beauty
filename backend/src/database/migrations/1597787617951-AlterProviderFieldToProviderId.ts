8505c5658c9cbf1c0ebe3b1f6c931257ed03d75d
Leandro Soares <lsoares199658@gmail.com>
2020-08-19 21:24:27 -0300

    refactor: dealing with errors

--- backend/src/database/migrations/1597787617951-AlterProviderFieldToProviderId.ts
+++ backend/src/database/migrations/1597787617951-AlterProviderFieldToProviderId.ts
@@ -9,6 +9,7 @@ export default class AlterProviderFieldToProviderId1597787617951
   implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.dropColumn('appointments', 'provider');
+
     await queryRunner.addColumn(
       'appointments',
       new TableColumn({
@@ -22,11 +23,11 @@ export default class AlterProviderFieldToProviderId1597787617951
       'appointments',
       new TableForeignKey({
         name: 'AppointmentProvider',
-        columnNames: ['provider_id'],
-        referencedColumnNames: ['id'],
+        columnNames: ['provider_id'], // Vai receber a chave estrangeira
+        referencedColumnNames: ['id'], // Da coluna 'id', da tabela de 'users'
         referencedTableName: 'users',
-        onDelete: 'SET NULL',
-        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL', // Quando deletar esse provider, vai setar o 'provider_id' como 'null'
+        onUpdate: 'CASCADE', // Quando atualizar o id do provider, vai atualizar o provider_id, nas tabela que estao relacionadas, como a tabela 'users'
       }),
     );
   }
